e0cb8a3b12c586adb7fcc6a8ca4624f01816c69f
Daniel J <d4niel.djm@gmail.com>
2024-10-21 18:12:58 -0500

    clean psconfig

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,40 +1,41 @@
+# Invoke Starship
 Invoke-Expression (&starship init powershell)
-Import-Module posh-sshell
-Import-Module Terminal-Icons
-Import-Module z
 
-# PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
+# Import necessary modules (only if they are needed)
+$modules = @('posh-sshell', 'Terminal-Icons', 'z', 'PSFzf', 'posh-git')
+
+foreach ($module in $modules) {
+    if (Get-Module -ListAvailable -Name $module) {
+        Import-Module $module
+    }
+}
+
+# PSReadLine settings
+Set-PSReadLineOption -EditMode Emacs -BellStyle None -PredictionSource History -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
 
-# Fzf
-Import-Module PSFzf
+# Fzf options
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# Functions as Aliases
-function fetch { & git fetch $args }
-function clone { & git clone $args }
-function pull { & git pull $args }
-function push { & git push $args }
+# Git alias functions
+$gitFunctions = @(
+    'fetch', 'clone', 'pull', 'push', 'add'
+)
+
+foreach ($func in $gitFunctions) {
+    Set-Alias $func "git $func"
+}
+
 function gco { & git checkout $args }
 function gcob { & git checkout -b $args }
 function stat { & git status $args }
-function add { & git add $args }
 function gcm { & git commit $args }
 function log { & git lg }
-function gam { 
-    & git add .
-    & git commit -m $args 
+function gam {
+    & git add .; & git commit -m $args
 }
 
-function pn {
-    & pnpm $args
-}
-
-# Alias
+# Alias for commands
 Set-Alias vim nvim
 Set-Alias ll ls
 Set-Alias g git
@@ -46,22 +47,19 @@ Set-Alias sudo admin
 
 # Utilities
 function which ($command) {
-    Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Get-Command -Name $command -ErrorAction SilentlyContinue | 
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-function .. { & cd .. }
-function ... { 
-    & cd .. 
-    & cd .. 
-}
+function .. { cd .. }
+function ... { cd ..; cd .. }
 
 function admin {
     Start-Process wt -Verb RunAs
 }
 
 function psconfig {
-    code $env:USERPROFILE\.config\powershell\user_profile.ps1
+    code "$env:USERPROFILE\.config\powershell\user_profile.ps1"
 }
 
 function touch {
@@ -71,35 +69,29 @@ function touch {
     if (Test-Path $Path) {
         # Update the timestamp
         (Get-Item $Path).LastWriteTime = Get-Date
-    }
-    else {
+    } else {
         # Create a new empty file
-        New-Item -Path $Path -ItemType File
+        New-Item -Path $Path -ItemType File -ErrorAction SilentlyContinue
     }
 }
 
 # Random. DON'T READ.
 function genshin {
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     iex "&{$((New-Object System.Net.WebClient).DownloadString('https://gist.github.com/MadeBaruna/1d75c1d37d19eca71591ec8a31178235/raw/getlink.ps1'))} global"
 }
 
 function zzz {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object Net.WebClient).DownloadString("https://zzz.rng.moe/scripts/get_signal_link_os.ps1")
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+    Invoke-Expression (New-Object Net.WebClient).DownloadString("https://zzz.rng.moe/scripts/get_signal_link_os.ps1")
 }
 
-#Import Posh-Git after aliases so it recognize them
-Import-Module posh-git
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
+# Import the Chocolatey Profile for tab completion
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
+if (Test-Path $ChocolateyProfile) {
     Import-Module "$ChocolateyProfile"
 }
 
+# Final message
 echo "You beautiful bean, PS is ready for your magic."
